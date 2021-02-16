2de8ba3001a0bdd49b3955c680b11be7f09006fd
Akash Sharma <akash2909as@gmail.com>
2021-02-16 13:15:16 +0530

    create and save product

--- backend/controllers/product.js
+++ backend/controllers/product.js
@@ -1,4 +1,16 @@
-exports.getProducts = (req , res) => {
+const Product = require('../models/product')
+
+
+exports.newProduct = async (req, res, next) => {
+  const product = await Product.create(req.body)
+
+  res.status(201).json({
+    success:true,
+    product
+  })
+}
+
+exports.getProducts = (req, res) => {
   res.status(200).json({
     message:"products"
   })
--- backend/routes/product.js
+++ backend/routes/product.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-const { getProducts } = require("../controllers/product");
+const { newProduct,getProducts } = require("../controllers/product");
 
 router.get("/products", getProducts);
+router.post("/products/new", newProduct);
 
 module.exports = router;
