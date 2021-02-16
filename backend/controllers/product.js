fb4c5ab8123ba29f2415dd489472d1d00bd2bccc
Akash Sharma <akash2909as@gmail.com>
2021-02-16 13:52:13 +0530

    get allproducts and productbyid

--- backend/controllers/product.js
+++ backend/controllers/product.js
@@ -10,8 +10,28 @@ exports.newProduct = async (req, res, next) => {
   })
 }
 
-exports.getProducts = (req, res) => {
+exports.getProducts = async (req, res) => {
+  const products = await Product.find()
+
   res.status(200).json({
-    message:"products"
+    success: true,
+    count: products.length,
+    products
+  })
+}
+
+exports.getSingleProduct = async (req, res, next) => {
+  const product = await Product.findById(req.params.id)
+
+  if (!product) {
+    return res.status(404).json({
+      success: false,
+      message:'Product not found'
+   })
+  }
+
+  res.status(200).json({
+    success: true,
+    product
   })
 }
--- backend/routes/product.js
+++ backend/routes/product.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
-const { newProduct,getProducts } = require("../controllers/product");
+const { newProduct,getProducts, getSingleProduct } = require("../controllers/product");
 
 router.get("/products", getProducts);
 router.post("/products/new", newProduct);
+router.get('/product/:id', getSingleProduct)
 
 module.exports = router;
