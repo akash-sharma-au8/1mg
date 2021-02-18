e64479c6fc1a27748a05368c4ee344b56bafbef7
Akash Sharma <akash2909as@gmail.com>
2021-02-18 19:18:31 +0530

    fixed server bug

--- backend/app.js
+++ backend/app.js
@@ -6,8 +6,10 @@ const fileUpload = require("express-fileupload");
 const errorMiddleware = require("./Middlewares/error");
 const path = require('path')
 const cors = require('cors')
+const env = require('dotenv')
 
 
+env.config({path: 'backend/config/config.env'})
 
 app.use(cors())
 app.use(express.json());
@@ -22,9 +24,9 @@ const orderRoutes = require("./routes/order");
 const paymentRoutes = require('./routes/payment');
 
 // Routing
-app.get("/", (req, res) => {
-  res.status(200).json({ message: "Health check" });
-});
+// app.get("/", (req, res) => {
+//   res.status(200).json({ message: "Health check" });
+// });
 app.use("/api", productRoutes);
 app.use("/api", authRoutes);
 app.use("/api", orderRoutes);
--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ cloudinary.config({
 })
 
 app.listen(process.env.PORT, () => {
-  console.log(`Server is running on ${process.env.PORT}`);
+  console.log(`Server is running on ${process.env.PORT}in  ${process.env.NODE_ENV}`);
 }) 
 
 // Handle Unhandled Promise rejections
